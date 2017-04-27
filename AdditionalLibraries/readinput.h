49594d6ee693c12030e2addc7bae4db682bc04ef
Arnab Bag <amiarnabbolchi@gmail.com>
2017-04-27 20:26:51 +0530

    Create readinput.h

---
+++ AdditionalLibraries/readinput.h
@@ -0,0 +1,10 @@
+#ifndef READINPUT
+#define READINPUT
+
+#include <string>
+#include <cstdlib>
+#include "CSVparser.hpp"
+
+int ReadFile(std::string myfile, float *imgdata, int M, int N);
+
+#endif // READINPUT
