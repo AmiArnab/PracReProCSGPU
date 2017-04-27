6722878252890b968b6fd6096f352214da63debd
Arnab Bag <amiarnabbolchi@gmail.com>
2017-04-27 20:27:10 +0530

    Add files via upload

---
+++ AdditionalLibraries/readinput.cpp
@@ -0,0 +1,20 @@
+#include "readinput.h"
+
+int ReadFile(std::string myfile, float *imgdata, int M, int N)
+{
+	csv::Parser file(myfile.c_str());
+
+	for (int i = 0; i < N; ++i)
+	{
+		imgdata[i] = 0;
+	}
+
+	for (int i = 1; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			imgdata[(i*N) + j] = std::strtof((file[i-1][j]).c_str(),0);
+		}
+	}
+	return 0;
+}
